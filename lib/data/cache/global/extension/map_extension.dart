92b516cdd6f8e1ab2ddc262f03ca216e1f3f35b9
idonun <mc.natsu831@gmail.com>
2021-02-01 21:49:51 +0900

    [fix] Fix Map extesion methods

--- lib/data/cache/global/extension/map_extension.dart
+++ lib/data/cache/global/extension/map_extension.dart
@@ -2,29 +2,29 @@ import 'package:clean_architecture_sample/data/cache/global/datastate/data_state
 import 'package:clean_architecture_sample/data/cache/global/datastate/paging_data_state.dart';
 import 'package:rxdart/rxdart.dart';
 
-extension MapBehaviorSubjectListDataStateExtension<K> on Map<K, BehaviorSubject<PagingDataState>> {
-  BehaviorSubject<PagingDataState> getOrCreate(final K key) {
-    return getOrCreateSeeded(key, () => const PagingDataState.normal(false, 1));
+extension MapBehaviorSubjectListDataStateExtension on Map<String, BehaviorSubject<PagingDataState>> {
+  BehaviorSubject<PagingDataState> getOrCreate(final String key) {
+    return getOrCreateSeeded(key, const PagingDataState.normal(false, 1));
   }
 }
 
-extension MapBehaviorSubjectDataStateExtension<K> on Map<K, BehaviorSubject<DataState>> {
-  BehaviorSubject<DataState> getOrCreate(final K key) {
-    return getOrCreateSeeded(key, () => const DataState.normal());
+extension MapBehaviorSubjectDataStateExtension on Map<String, BehaviorSubject<DataState>> {
+  BehaviorSubject<DataState> getOrCreate(final String key) {
+    return getOrCreateSeeded(key, const DataState.normal());
   }
 }
 
-extension MapBehaviorSubjectExtension<K, V> on Map<K, BehaviorSubject<V>> {
-  BehaviorSubject<V> getOrCreateSeeded(final K key, final V Function() seed) {
-    return getOrPut(key, () => BehaviorSubject.seeded(seed()));
+extension MapBehaviorSubjectExtension<V> on Map<String, BehaviorSubject<V>> {
+  BehaviorSubject<V> getOrCreateSeeded(final String key, final V seed) {
+    return getOrPut(key, BehaviorSubject.seeded(seed));
   }
 }
 
-extension MapExtension<K, V> on Map<K, V> {
-  V getOrPut(final K key, final V Function() orElse) {
+extension MapExtension<V> on Map<String, V> {
+  V getOrPut(final String key, final V orElse) {
     var value = this[key];
     if (value == null) {
-      value = orElse();
+      value = orElse;
       this[key] = value;
     }
     return value;
